6dd23c62b7720430ddcc456ba0a5d9af64c411dd
Alexey <slow.net@mail.ru>
2022-10-18 17:19:49 +0300

    Первая задача ДЗ семинара 2

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int randomNumber = GetRandomNumber(100, 1000);
+
+int secondNumber = SecondNumber(randomNumber);
+
+Console.WriteLine(randomNumber);
+
+Console.WriteLine(secondNumber);
+
+int GetRandomNumber(int minValue, int maxValue)
+
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue, maxValue);
+    return randomNumber;
+}
+int SecondNumber(int randomNumber)
+{
+    int secondNumber = randomNumber /10 % 10;
+    
+    return secondNumber;
+}
