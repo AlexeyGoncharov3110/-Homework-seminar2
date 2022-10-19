70b41575a57ec37992a7614b0842ea8b3b033bd3
Alexey <slow.net@mail.ru>
2022-10-19 16:28:22 +0300

    Решение второй задачи ДЗ семинара 2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine(" Введите целое число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number;
+int count = 0;
+while (number1 > 0)
+{
+    number1 /= 10;
+    count++;
+}
+int[] array = new int[count];
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        int lastValue = number % 10;
+        array[i] = lastValue;
+        number = number / 10;
+    }
+    Console.WriteLine(array[2]);
+}
+
+
