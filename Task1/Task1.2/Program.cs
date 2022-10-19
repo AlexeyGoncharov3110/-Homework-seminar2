61893962fd1affde9d7c9993859fc8813d8518c3
Alexey <slow.net@mail.ru>
2022-10-19 11:35:15 +0300

    Второе решение первой задачи ДЗ семинара 2

---
+++ Task1/Task1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(" Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = SecondNumber(number);
+
+if(number>100 && number<1000)
+{
+Console.WriteLine(secondNumber);
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
+
+int SecondNumber(int number)
+{
+    int secondNumber = number /10 % 10;
+    
+    return secondNumber;
+}
