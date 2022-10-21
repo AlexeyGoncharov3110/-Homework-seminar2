56de13bd2b9347f064680119e8046c8c63ffba06
Alexey <slow.net@mail.ru>
2022-10-21 15:48:47 +0300

    Решение 3 -ей задачи ДЗ семинара 2 , вторым способом

---
+++ Task3.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine(" Введите число от 1 до 7 ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+DayOfTheWeek(number);
+
+bool DayOfTheWeek(int arg)
+{
+    return number == 6 | number == 7;
+}
